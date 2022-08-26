aaf63893509948598ba6c87e9cc6af159075d00c
siawkz <siawkz@gmail.com>
2022-08-27 00:55:29 +0800

    Change lazygit term toggle

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -196,23 +196,7 @@ end
 
 M.toggle_lazygit = function()
   local Terminal = require('toggleterm.terminal').Terminal
-  local lazygit = Terminal:new({
-    cmd = "lazygit",
-    dir = "git_dir",
-    direction = "float",
-    float_opts = {
-      border = "double",
-    },
-    -- function to run on opening the terminal
-    on_open = function(term)
-      vim.cmd("startinsert!")
-      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-    end,
-    -- function to run on closing the terminal
-    on_close = function(term)
-      vim.cmd("Closing terminal")
-    end,
-  })
+  local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
   lazygit:toggle()
 end
 
