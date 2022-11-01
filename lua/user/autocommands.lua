221c7e6823ebcb801eb455cfd6ccbc8543076ec3
siawkz <siawkz@gmail.com>
2022-11-01 23:02:57 +0800

    Remove _lvim_colorscheme

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -4,7 +4,6 @@ local create_aucmd = vim.api.nvim_create_autocmd
 
 M.config = function()
   vim.api.nvim_clear_autocmds { pattern = "lir", group = "_filetype_settings" }
-  vim.api.nvim_clear_autocmds { pattern = "*", group = "_lvim_colorscheme" }
   vim.api.nvim_create_augroup("_lvim_user", {})
   -- Autocommands
   create_aucmd("TextYankPost", {
