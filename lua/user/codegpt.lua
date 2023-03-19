ad810e68387d774f646387f41aa99ccf0e51422c
siawkz <siawkz@gmail.com>
2023-03-19 16:17:20 +0800

    Change max_tokens

--- lua/user/codegpt.lua
+++ lua/user/codegpt.lua
@@ -8,18 +8,22 @@ M.config = function()
 
 	-- TODO: Change to gpt-4 later
 	local model = "gpt-3.5-turbo"
+	local max_tokens = 16384
 
 	vim.g["codegpt_commands_defaults"] = {
 		["completion"] = {
 			model = model,
+			max_tokens = max_tokens,
 			user_message_template = "I have the following {{language}} code snippet: ```{{filetype}}\n{{text_selection}}```\nComplete the rest. Use best practices and write really good documentation. {{language_instructions}} Only return the code snippet and nothing else.",
 			language_instructions = {
 				cpp = "Use modern C++ features.",
 				java = "Use modern Java syntax. Use var when applicable.",
+				csharp = "Use modern C# syntax. Use var when applicable.",
 			},
 		},
 		["code_edit"] = {
 			model = model,
+			max_tokens = max_tokens,
 			user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\n{{command_args}}. {{language_instructions}} Only return the code snippet and nothing else.",
 			language_instructions = {
 				cpp = "Use modern C++ syntax.",
@@ -27,19 +31,27 @@ M.config = function()
 		},
 		["explain"] = {
 			model = model,
+			max_tokens = max_tokens,
 			user_message_template = "Explain the following {{language}} code: ```{{filetype}}\n{{text_selection}}``` Explain as if you were explaining to another developer.",
 			callback_type = "text_popup",
 		},
 		["doc"] = {
 			model = model,
+			max_tokens = max_tokens,
 			user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nWrite really good documentation using best practices for the given language. Attention paid to documenting parameters, return types, any exceptions or errors. {{language_instructions}} Only return the code snippet and nothing else.",
 			language_instructions = {
 				cpp = "Use doxygen style comments for functions.",
 				java = "Use JavaDoc style comments for functions.",
 			},
 		},
+		["md"] = {
+			model = model,
+			max_tokens = max_tokens,
+			user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nWrite really good markdown documentation using best practices for the given language.",
+		},
 		["opt"] = {
 			model = model,
+			max_tokens = max_tokens,
 			user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nOptimize this code. {{language_instructions}} Only return the code snippet and nothing else.",
 			language_instructions = {
 				cpp = "Use modern C++.",
@@ -47,6 +59,7 @@ M.config = function()
 		},
 		["tests"] = {
 			model = model,
+			max_tokens = max_tokens,
 			user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nWrite really good unit tests using best practices for the given language. {{language_instructions}} Only return the unit tests. Only return the code snippet and nothing else. ",
 			callback_type = "code_popup",
 			language_instructions = {
@@ -56,6 +69,7 @@ M.config = function()
 		},
 		["chat"] = {
 			model = model,
+			max_tokens = max_tokens,
 			system_message_template = "You are a general assistant to a software developer.",
 			user_message_template = "{{command_args}}",
 			callback_type = "text_popup",
