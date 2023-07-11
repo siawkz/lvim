9523246c0fbc587b3c88784c42e0a36e619f5eb3
siawkz <siawkz@gmail.com>
2023-07-11 23:00:45 +0800

    Change key mapping of vim-visual-multi

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -374,6 +374,12 @@ M.config = function()
 		{
 			"mg979/vim-visual-multi",
 			branch = "master",
+			init = function()
+				vim.g.VM_maps = {
+					["Add Cursor Down"] = "<C-j>",
+					["Add Cursor Up"] = "<C-k>",
+				}
+			end,
 		},
 		{
 			"olexsmir/gopher.nvim",
