d5701a0074242878fd6df1bcc58585eca4382776
siawkz <siawkz@gmail.com>
2023-07-31 22:42:59 +0800

    Remove plugin extra settings

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -438,24 +438,6 @@ M.config = function()
 					debugger_cmd = { "js-debug-adapter" },
 					adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
 				})
-				for _, language in ipairs({ "typescript", "javascript" }) do
-					require("dap").configurations[language] = {
-						{
-							type = "pwa-node",
-							request = "launch",
-							name = "Launch file",
-							program = "${file}",
-							cwd = "${workspaceFolder}",
-						},
-						{
-							type = "pwa-node",
-							request = "attach",
-							name = "Attach",
-							processId = require("dap.utils").pick_process,
-							cwd = "${workspaceFolder}",
-						},
-					}
-				end
 			end,
 		},
 		{
