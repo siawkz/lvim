b9303f18eec959cfa45bc5682528946fe0692eb6
siawkz <siawkz@gmail.com>
2023-09-08 10:18:55 +0800

    Add better-escape and vim-dirdiff

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -148,6 +148,20 @@ M.config = function()
 				})
 			end,
 		},
+		{
+			"max397574/better-escape.nvim",
+			event = "InsertEnter",
+			config = function()
+				require("better_escape").setup({
+					mapping = { "jk", "jj" },
+					timeout = vim.o.timeoutlen,
+				})
+			end,
+		},
+		{
+			"will133/vim-dirdiff",
+			cmd = "DirDiff",
+		},
 		{ "mfussenegger/nvim-jdtls", ft = "java" },
 		{
 			"danymat/neogen",
